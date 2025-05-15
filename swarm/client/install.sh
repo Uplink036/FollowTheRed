cabdb2c49d339b71b3321f094c165338881cf0b8
Uplink036 <oliversjodin02@gmail.com>
2025-05-15 11:17:08 +0000

    Update install to allow for IP

--- swarm/client/install.sh
+++ swarm/client/install.sh
@@ -1,4 +1,4 @@
-pip install -r req.txt
-sudo apt update 
-sudo apt upgrade -y
-sudo apt install ffmpeg libsm6 libxext6 -y
+IP=$1
+ssh -t jetbot@$IP   pip install -r req.txt;
+                    sudo apt update;
+                    sudo apt install ffmpeg libsm6 libxext6 -y
